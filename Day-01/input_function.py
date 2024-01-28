252ef13e318f939ebb6d0029448d0d9fc368c5f1
Durgesh Samariya <samariya.durgesh@gmail.com>
2024-01-28 23:08:14 +1100

    Day 01 added.

---
+++ Day-01/input_function.py
@@ -0,0 +1,21 @@
+"""
+Write some code that multiples together the two numbers in the input area.
+"""
+
+num1 = int(input())
+num2 = int(input())
+
+print(num1 * num2)
+
+
+"""
+Write a program that calculates and outputs the number of characters in any name.
+
+# Provide any name in the input pane below.
+# That value can be accessed using the input() function.
+# Don't put anything inside the input() function!
+"""
+
+name = input()
+
+print(len(name))
